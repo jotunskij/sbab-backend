9ff0ed8de810fe9811aa450982abd5867380ee1e
Jens Tinglev <jens.tinglev@gmail.com>
2023-09-17 12:08:42 +0200

    Update LineAndStopServiceImpl.java

--- src/main/java/se/sbab/sbabbe/Services/LineAndStopServiceImpl.java
+++ src/main/java/se/sbab/sbabbe/Services/LineAndStopServiceImpl.java
@@ -25,32 +25,38 @@ public class LineAndStopServiceImpl implements ILineAndStopService {
 
     @Override
     public List<TopLine> getTop10Lines() {
+        // Get all line nr -> stop nrs connections
         BaseResponse<LineStop> lineStops = repository.getLineStops();
         List<TopLine> topLines = new ArrayList<TopLine>();
 
+        // Get all stop names
         List<StopPoint> distictStops = getDistinctStops();
 
+        // Loop every line
         for (var lineStop : lineStops.responseData.result) {
+            // Get the corresponding StopPoint for this stop nr
             Optional<StopPoint> stop = getStopPointForLineNumber(
                 distictStops, 
                 lineStop.journeyPatternPointNumber
             );
 
+            // Ignore missing stops
             if (!stop.isPresent()) {
                 continue;
             }
             String stopName = stop.get().stopPointName;
             if (!lineNumberAlreadyInList(topLines, lineStop.lineNumber)) {
+                // Create a new top line entry for the current line nr
                 topLines.add(new TopLine(lineStop.lineNumber));
             }
-            else {
-                TopLine matchedLine = getTopLineWithLineNumber(topLines, lineStop.lineNumber);
-                topLines.get(topLines.indexOf(matchedLine)).stopNames.add(stopName);
-            }
+            // Find the top line entry for this line nr and add the stop
+            TopLine matchedLine = getTopLineWithLineNumber(topLines, lineStop.lineNumber);
+            topLines.get(topLines.indexOf(matchedLine)).stopNames.add(stopName);
         }
 
-        // Sort by lines array size, and limit to 10
+        // Sort by stops array size
         Collections.sort(topLines);
+        // Limit nr of items
         return topLines.subList(0, ResponseLimit - 1);
     }
 
